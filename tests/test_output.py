f62634bd330a8d2dd39370cd4dcccb8a97468495
Niklas Mikael <nikke.larsson@gmail.com>
2022-10-30 16:01:31 +0200

    Properly test `commandsheet.output.format_section_content`

--- tests/test_output.py
+++ tests/test_output.py
@@ -29,17 +29,50 @@ def test_format_section_heading():
     assert format_section_heading(text, index=None, surround=None) == 'heading'
 
 
-def test_format_section_content():
+def test_format_section_content_with_short_description():
     cmd = 'ls --long'
     dsc = 'List in a long format'
-    indent = 50
     fillchar = '.'
+    indent = len(cmd) + 5
     max_width = 85
-    expected = f'{cmd:{fillchar}<{indent}} {dsc}'
-    assert format_section_content(
+
+    result = format_section_content(
         cmd,
         dsc,
         indent=indent,
         fillchar=fillchar,
         max_width=max_width
-    ) == expected
+    )
+
+    expected = 'ls --long..... List in a long format'
+    assert result == expected
+
+
+def test_format_section_content_with_long_description():
+    cmd = 'ls --long'
+    dsc = 'List in a long format. You can also use the -l option.'
+    fillchar = '.'
+    indent = len(cmd) + 5
+
+    # We don't need to actually write a longer description
+    # to test against a 'longer' description (we could if
+    # we'd like to, but that is unnecessary). Instead, we
+    # can simply reduce the value of the ``max_width``
+    # parameter.
+    max_width = 40
+
+    result = format_section_content(
+        cmd,
+        dsc,
+        indent=indent,
+        fillchar=fillchar,
+        max_width=max_width
+    )
+
+    expected = (
+        'ls --long..... List in a long format. You can also use\n'
+        '               the -l option.'
+    )
+    assert result == expected
+
+
